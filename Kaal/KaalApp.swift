4f809df70db9f9a11ba3ac4e1a25a54a14bc61b0
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2023-12-05 13:56:16 -0800

    Initial Commit

---
+++ Kaal/KaalApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaalApp.swift
+//  Kaal
+//
+//  Created by Chetan Dhowlaghar on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct KaalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
