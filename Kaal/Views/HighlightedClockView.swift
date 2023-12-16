5812494efe266e151d941f0735fe8409a0a7ab96
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2023-12-16 01:19:20 -0800

    Clock view

---
+++ Kaal/Views/HighlightedClockView.swift
@@ -0,0 +1,18 @@
+//
+//  HighlightedClockView.swift
+//  Kaal
+//
+//  Created by Chetan Dhowlaghar on 12/14/23.
+//
+
+import SwiftUI
+
+struct HighlightedClockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HighlightedClockView()
+}
