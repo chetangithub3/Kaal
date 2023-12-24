6169ed2fce035730b46cde577fa5f4d0293a7eb9
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2023-12-24 15:11:10 -0800

    clocks revamped

--- Kaal/Views/HighlightedClockView.swift
+++ Kaal/Views/HighlightedClockView.swift
@@ -90,7 +90,9 @@ struct Highlighted24HourClockView: View {
                             self.percentage = 1.0
                         }
                     }
-                
+                durationString(from: range)
+                    .font(.title)
+                    .bold()
                 Image(systemName: "circle")
                     .font(.callout)
                     .foregroundColor(.blue)
@@ -129,7 +131,43 @@ struct Highlighted24HourClockView: View {
             }
         }
     }
-    
+    func durationString(from range: ClosedRange<Date>) -> Text {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.minute, .hour], from: range.lowerBound, to: range.upperBound)
+        
+        if let hours = components.hour, let minutes = components.minute {
+                   if hours == 0 {
+                      return
+                           Text("\(minutes)")
+                               .bold()
+                               .font(.title)
+                       +
+                           Text(" mins")
+                               .font(.caption)
+                       
+                       
+                   } else {
+                       return
+                           Text("\(hours)")
+                               .bold()
+                               .font(.title)
+                       +
+                           Text(" hrs")
+                               .font(.caption)
+                       +
+                           Text(" \(minutes)")
+                               .bold()
+                               .font(.title)
+                       +
+                           Text(" mins")
+                               .font(.caption)
+                       
+                   }
+               }
+    return  Text("Unknown duration")
+       
+    }
+
 }
 
 struct Highlighted12HourClockView: View {
@@ -209,6 +247,7 @@ struct Highlighted12HourClockView: View {
                             self.percentage = 1.0
                         }
                     }
+                durationString(from: range)
                 Image(systemName: "circle")
                     .font(.callout)
                     .foregroundColor(.blue)
@@ -248,7 +287,43 @@ struct Highlighted12HourClockView: View {
             }
         }
     }
-    
+    func durationString(from range: ClosedRange<Date>) -> Text {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.minute, .hour], from: range.lowerBound, to: range.upperBound)
+        
+        if let hours = components.hour, let minutes = components.minute {
+                   if hours == 0 {
+                      return
+                           Text("\(minutes)")
+                               .bold()
+                               .font(.title)
+                       +
+                           Text(" mins")
+                               .font(.caption)
+                       
+                       
+                   } else {
+                       return
+                           Text("\(hours)")
+                               .bold()
+                               .font(.title)
+                       +
+                           Text(" hrs")
+                               .font(.caption)
+                       +
+                           Text(" \(minutes)")
+                               .bold()
+                               .font(.title)
+                       +
+                           Text(" mins")
+                               .font(.caption)
+                       
+                   }
+               }
+    return  Text("Unknown duration")
+       
+    }
+
 }
 
 #Preview {
