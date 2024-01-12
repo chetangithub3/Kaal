a4475f53780ab9e55640fe036775001a3f904abd
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2024-01-12 00:17:38 -0800

    enhancements, later version code

--- Kaal/Protocols.swift
+++ Kaal/Protocols.swift
@@ -47,4 +47,37 @@ extension Clock {
         return Text("Unknown duration")
         
     }
+    
+    func calculateHourAndMinuteAngles() -> (Double, Double) {
+        let currentDate = Date()
+        let calendar = Calendar.current
+
+        let hour = calendar.component(.hour, from: currentDate)
+        let minute = calendar.component(.minute, from: currentDate)
+
+        // Calculate angles based on your specific requirements
+        let hourAngle = Double(hour % 12) / 12.0 * 360.0
+        let minuteAngle = Double(minute) / 60.0 * 360.0
+        
+        return (hourAngle, minuteAngle)
+    }
+    
+    func calculateHourAngle(time: Date) -> Double {
+         let calendar = Calendar.current
+         let hour = calendar.component(.hour, from: time) % 12
+         let minute = calendar.component(.minute, from: Date())
+         return Double(hour * 30 + minute / 2)
+     }
+
+     func calculateMinuteAngle(time: Date) -> Double {
+         let calendar = Calendar.current
+         let minute = calendar.component(.minute, from: time)
+         return Double(minute * 6)
+     }
+    func calculateSecondAngle(time: Date) -> Double {
+        let calendar = Calendar.current
+        let second = calendar.component(.second, from: time)
+        let adjustedSecond = second == 60 ? 0 : Double(second) // Adjust for the 60th second
+        return adjustedSecond * 6
+     }
 }
