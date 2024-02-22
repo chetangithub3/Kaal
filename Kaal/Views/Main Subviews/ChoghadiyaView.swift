988f8fb6c8046cedb8260505c39fbdeb0058d78f
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2024-02-22 01:29:51 -0800

    UI update

--- Kaal/Views/Main Subviews/ChoghadiyaView.swift
+++ Kaal/Views/Main Subviews/ChoghadiyaView.swift
@@ -18,10 +18,22 @@ struct ChoghadiyaView: View {
                     let choghadiya = viewModel.choghadiya
                     CustomDatePickerView(date: $date, timezone: viewModel.timezone)
                     ScrollView {
+                        HStack{
+                            Image(systemName: "sun.max.fill")
+                            Text("Day Choghadiya")
+                            Spacer()
+                        }.foregroundColor(getTintColor())
+                        .font(.title3).bold()
+                        
                         ForEach(choghadiya!.dayChoghadiya.gadiyas, id: \.1.upperBound) { gadiya in
                            GadiyaView(gadiya: gadiya)
                         }
-                        
+                        HStack{
+                            Image(systemName: "moon.fill")
+                            Text("Night Choghadiya")
+                            Spacer()
+                        }.foregroundColor(Color.blue)
+                        .font(.title3).bold()
                         ForEach(choghadiya!.nightChoghadiya.gadiyas, id: \.1.lowerBound) { gadiya in
                             GadiyaView(gadiya: gadiya)
                         }
@@ -36,6 +48,7 @@ struct ChoghadiyaView: View {
                 }
                 
             }.navigationTitle("Choghadiya")
+                .navigationBarTitleDisplayMode(.inline)
                 .onChange(of: date) { oldValue, newValue in
                     viewModel.getChoghadiyas(on: newValue)
                 }
@@ -45,7 +58,9 @@ struct ChoghadiyaView: View {
 }
 
 struct GadiyaView: View {
+    
     var gadiya: (String, ClosedRange<Date>)
+    @State private var currentTimeWithinRange = false
     
     var body: some View {
         
@@ -53,17 +68,24 @@ struct GadiyaView: View {
             HStack(spacing: 0){
                 VStack(alignment: .leading){
                     Spacer()
-                    Text(gadiya.0)
-                        .font(.title2)
-                        .frame(width: getScreenBounds().width * 0.25)
-                        .bold()
+                    HStack(spacing: 2){
+                        if currentTimeWithinRange{
+                            Image(systemName: "stopwatch.fill")
+                                .symbolEffect(.variableColor.iterative)
+                                .symbolVariant(.slash)
+                        }
+                        
+                        Text(gadiya.0)
+                            .font(.title2)
+                            .bold()
+                    }
                     Text(Choghadiya(rawValue: gadiya.0)?.nature.title ?? "")
                         .font(.subheadline)
-                        .frame(width: getScreenBounds().width * 0.25)
                     Spacer()
-                }.foregroundColor(.white)
+                }.frame(width: getScreenBounds().width * 0.25)
+                    .foregroundColor(.white)
                     .background(Choghadiya(rawValue: gadiya.0)?.nature.color)
-                    
+                
                 Spacer()
                 VStack(alignment: .leading){
                     Text("Starts at:").font(.subheadline)
@@ -71,7 +93,7 @@ struct GadiyaView: View {
                         .font(.title2).bold()
                 }.foregroundColor(.black)
                     .padding()
-    
+                
                 Spacer()
                 VStack(alignment: .leading){
                     Text("Ends at:").font(.subheadline)
@@ -79,11 +101,14 @@ struct GadiyaView: View {
                         .font(.title2).bold()
                 }.foregroundColor(.black)
                     .padding()
-                   
+                
             }
         }
         .background(Choghadiya(rawValue: gadiya.0)?.nature.color.opacity(0.2))
-            .cornerRadius(8)
-            .padding(.bottom)
+        .cornerRadius(8)
+        .padding(.bottom)
+        .onAppear {
+            currentTimeWithinRange = gadiya.1.contains(Date())
+        }
     }
 }
