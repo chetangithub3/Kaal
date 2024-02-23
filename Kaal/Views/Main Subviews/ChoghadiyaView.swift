f176faf3b161438c773dac3f759af7f4f84a82f2
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2024-02-23 15:47:16 -0800

    gadiya UI update

--- Kaal/Views/Main Subviews/ChoghadiyaView.swift
+++ Kaal/Views/Main Subviews/ChoghadiyaView.swift
@@ -57,75 +57,28 @@ struct ChoghadiyaView: View {
     }
 }
 
-struct GadiyaView: View {
-    
-    var gadiya: (String, ClosedRange<Date>)
-    @State private var currentTimeWithinRange = false
-    @State private var timeRemaining = TimeInterval(0)
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
- 
-    @State var remainingTime = ""
-    var body: some View {
-        
-        VStack(alignment: .leading){
-            HStack(spacing: 0){
-                VStack(alignment: .leading){
-                    Spacer()
-                    HStack(spacing: 2){
-                        Text(gadiya.0)
-                            .font(.title2)
-                            .bold()
+
+
+struct Glow: ViewModifier {
+    var shouldGlow: Bool
+    @State var throb = false
+    func body(content: Content) -> some View {
+        ZStack{
+            if shouldGlow{
+                content
+                    .blur(radius: throb ? 10 : 0)
+                    .animation(.easeInOut(duration: 0.5).repeatForever(), value: throb)
+                    .onAppear {
+                        throb.toggle()
                     }
-                    Text(Choghadiya(rawValue: gadiya.0)?.nature.title ?? "")
-                        .font(.subheadline)
-                    Spacer()
-                }.frame(width: getScreenBounds().width * 0.25)
-                    .foregroundColor(.white)
-                    .background(Choghadiya(rawValue: gadiya.0)?.nature.color)
-                
-                Spacer()
-                VStack(alignment: .leading){
-                    Text("Starts at:").font(.subheadline)
-                    Text(gadiya.1.lowerBound.toStringVersion())
-                        .font(.title2).bold()
-                        .lineLimit(1)
-                        .minimumScaleFactor(0.5)
-                }.foregroundColor(.black)
-                    .padding()
-                
-                Spacer()
-                VStack(alignment: .leading){
-                    Text("Ends at:").font(.subheadline)
-                    Text(gadiya.1.upperBound.toStringVersion())
-                        .font(.title2).bold()
-                        .lineLimit(1)
-                        .minimumScaleFactor(0.5)
-                }.foregroundColor(.black)
-                    .padding()
-                
             }
+          
+            content
         }
-        .background(Choghadiya(rawValue: gadiya.0)?.nature.color.opacity(0.2))
-        .cornerRadius(8)
-        .padding(.bottom)
-        .onReceive(timer, perform: { _ in
-            currentTimeWithinRange = gadiya.1.contains(Date())
-            updateTimer()
-        })
     }
-    
-    private func updateTimer() {
-        if currentTimeWithinRange {
-            let currentDate = Date()
-            if gadiya.1.contains(currentDate) {
-                timeRemaining = gadiya.1.upperBound.timeIntervalSince(currentDate)
-            } else {
-                timeRemaining = 0
-            }
-        }
-        let minutes = Int(timeRemaining) / 60
-        let seconds = Int(timeRemaining) % 60
-        self.remainingTime = String(format: "%02d:%02d", minutes, seconds)
+}
+extension View{
+    func glow(shouldGlow: Bool) -> some View {
+        modifier(Glow(shouldGlow: shouldGlow))
     }
-    
 }
